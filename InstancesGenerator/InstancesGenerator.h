08b1a30c4ba67b7a6b9adcfa1a3afc3f67c4c01f
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-10-15 13:03:39 +0200

    Initial Commit

---
+++ InstancesGenerator/InstancesGenerator.h
@@ -0,0 +1,19 @@
+//
+//  InstancesGenerator.h
+//  InstancesGenerator
+//
+//  Created by Łukasz Dziedzic on 15/10/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstancesGenerator.
+FOUNDATION_EXPORT double InstancesGeneratorVersionNumber;
+
+//! Project version string for InstancesGenerator.
+FOUNDATION_EXPORT const unsigned char InstancesGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstancesGenerator/PublicHeader.h>
+
+
