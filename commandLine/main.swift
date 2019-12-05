3ff05b2b46cb0394257f13115520865a7d4d3e2b
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-12-05 15:52:30 +0100

    added

---
+++ commandLine/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  insgen
+//
+//  Created by Łukasz Dziedzic on 15/10/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+import Foundation
+
+
+class ConsoleIO {
+	enum OutputType {
+		case error
+		case standard
+	}
+	func writeMessage(_ message: String, to: OutputType = .standard) {
+		switch to {
+		case .standard:
+			print("\(message)")
+		case .error:
+			fputs("Error: \(message)\n", stderr)
+		}
+	}
+	
+	func printUsage() {
+		
+		let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
+		
+		writeMessage("usage:")
+		writeMessage("\(executableName) source.json target.json")
+	}
+}
+
+
+
+
+if  CommandLine.argc == 3 {
+//2
+	let sourceFileName = CommandLine.arguments[1]
+	let targetFileName = CommandLine.arguments[2]
+	ConsoleIO().writeMessage(sourceFileName)
+	let fileManager = FileManager()
+	let currentURL = URL(fileURLWithPath: fileManager.currentDirectoryPath)
+	let inputJsonFileURL = currentURL.appendingPathComponent(sourceFileName)
+	let outputJsonFileURL = currentURL.appendingPathComponent(targetFileName)
+	do {
+		let data = try Data(contentsOf: inputJsonFileURL)
+		print (data)
+		let ig = try InstancesCalculator<Double>(from: data)
+		print (ig)
+		//ig.instances.forEach({print ($0)})
+		try ig.exportJSON(to: outputJsonFileURL)
+	} catch let error {
+		ConsoleIO().writeMessage(error.localizedDescription, to: .error)
+	}
+} else {
+	ConsoleIO().writeMessage("Needs input and output file names")
+}
+//3
+
